08c155cde1fb21df5179cc29c9d2b2898dec598c
cechaney <chaneymail@gmail.com>
2014-09-08 11:11:10 -0500

    Changed to HSQLDB for easy attendee setup

--- SpringMVC-JPA-Eclipse/src/com/cec/restapijpa/web/ProfileController.java
+++ SpringMVC-JPA-Eclipse/src/com/cec/restapijpa/web/ProfileController.java
@@ -6,9 +6,13 @@ package com.cec.restapijpa.web;
 
 import com.cec.restapijpa.domain.Profile;
 import com.cec.restapijpa.services.ProfileService;
+
 import java.util.List;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,7 +33,21 @@ public class ProfileController {
         return profileService.findAll();
 		
 	}
-    
+	
+	@RequestMapping(value="/profile/generate", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody Profile generateProfile(
+			HttpServletRequest req, 
+			HttpServletResponse res){
+		
+		Profile profile = new Profile();
+
+		profile.setEmail(UUID.randomUUID()+"@email.com");
+		profile.setPassword(UUID.randomUUID().toString());
+
+        return profileService.create(profile);
+		
+	}	
+
 	@RequestMapping(value="/hello", method = RequestMethod.GET)
 	public @ResponseBody String sayHello(
 			HttpServletRequest req, 
