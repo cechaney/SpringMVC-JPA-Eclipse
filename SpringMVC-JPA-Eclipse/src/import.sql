8663cffd2bf14000c0574702f803fae2106e25d5
cechaney <chaneymail@gmail.com>
2014-09-08 11:53:16 -0500

    downgraded pom.xml to old version

--- SpringMVC-JPA-Eclipse/src/import.sql
+++
@@ -1,4 +0,0 @@
-insert into profile(email,password) values("test1@test.com","")
-insert into profile(email,password) values("test2@test.com","")
-insert into profile(email,password) values("test3@test.com","")
-insert into profile(email,password) values("test4@test.com","")
